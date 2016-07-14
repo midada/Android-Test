28f8343c4b60f8b59646b81a0bfefe9fd9a8e214
midada <sx.work@outlook.com>
2016-07-14 16:02:03 +0800

    TestCase

--- Appium/test_login.py
+++ Appium/test_login.py
@@ -3,17 +3,22 @@
 
 import os,sys
 import unittest
-import test_config
-from appium_swipe import MobileSwipe
 from time import sleep
+
 from appium import webdriver
 from unittest import TestCase
 from selenium.webdriver.common.by import By
 
+import test_config
+from appium_swipe import MobileSwipe
+from test_account import login,logout,register
+
 #set username data
-username = '13700338877'
+username = '18311446031'
 password = 'a123456'
-nickname = 'test13700338877'
+nickname = 'test13700338727'
+identifying_code = '1234'
+
 
 class TestAndroidJiuai(unittest.TestCase):
     
@@ -36,58 +41,16 @@ class TestAndroidJiuai(unittest.TestCase):
         sleep(3)
         self.assertEqual('.activity.MainActivity',self.driver.current_activity)
 
-    #Regiter: mobile register
-    #@unittest.skip("No run")
+    @unittest.skip("No run")
     def test_register(self):
-        #open register page
-        sleep(3)
-        self.driver.find_element_by_id("com.jiuai:id/rbPersonal").click()
-        self.driver.find_element_by_id("com.jiuai:id/rl_personal_bg").click()
-        self.driver.find_element_by_id("com.jiuai:id/tv_register").click()
+        register(self.driver,username,identifying_code,passwod,nickname)
 
-        #register:the first step
-        self.driver.find_element_by_id("com.jiuai:id/et_phoneNumber").send_keys(username)
-        self.driver.find_element_by_id("com.jiuai:id/tv_identifying_code").click()
-        self.driver.find_element_by_id("com.jiuai:id/et_identifying_code").send_keys("1234")
-        self.driver.find_element_by_id("com.jiuai:id/et_password").send_keys(password)
-        sleep(2)
-        self.driver.keyevent(66)
-        self.driver.find_element_by_id("com.jiuai:id/btn_reg_next").click()
-        
-
-        #register:the second setp
-        self.driver.find_element_by_id("com.jiuai:id/et_nickname").send_keys(nickname)
-        self.driver.find_element_by_id("com.jiuai:id/btn_reg_complete").click()
-        sleep(3)
-
-        #register succesful page
-        self.assertEqual(".activity.RegisterRecommendActivity",self.driver.current_activity)
-        self.driver.find_element_by_id("com.jiuai:id/btn_go_home").click
-
-    #Login:app username login
-    @unittest.skip("No Run")
+    #@unittest.skip("No Run")
     def test_login(self):
-
-        #open login page
-        sleep(4)
-        self.driver.find_element_by_id("com.jiuai:id/rbPersonal").click()
-        self.driver.find_element_by_id("com.jiuai:id/rl_personal_bg").click()
-
-        #input mobile and password
-        self.driver.find_element_by_id("com.jiuai:id/et_phoneNumber").send_keys("18311446031")
-        self.driver.find_element_by_id("com.jiuai:id/et_password").send_keys("a123456")
-        self.driver.find_element_by_id("com.jiuai:id/btn_common_login").click()
-
-        #Click the mask
-        self.driver.find_element_by_id("com.jiuai:id/linearLayout_mask").click()
-
-        #Screenshot: After the successs of the user login
-        self.driver.get_screenshot_as_file('login.png')
+        login(self.driver,username,password)
     
-    #MyWallet
     @unittest.skip("No Run")
-    def test_my_wallet(self):    
-        
+    def test_my_wallet(self):     
         self.driver.find_element_by_id("com.jiuai:id/option_my_wallet").click()
         self.driver.find_element_by_id("com.jiuai:id/ll_takeout_cash").click()
         self.driver.find_element_by_id("com.jiuai:id/et_cash_to_getout").send_keys("0.01")
