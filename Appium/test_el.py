51865cf93752ff82f156a7e9f36ebf47db6a551d
midada <sx.work@outlook.com>
2016-07-14 18:37:46 +0800

    locate element

---
+++ Appium/test_el.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,sys
+from time import sleep
+
+def el_click(driver,el):
+    return driver.find_element_by_id(el).click()
+
+def el_sk(driver,el,data):
+    return driver.find_element_by_id(el).send_keys(data)
