36f3442ce2c606e205ca6ab9831e207166a11b80
midada <sx.work@outlook.com>
2016-07-19 16:26:27 +0800

    TestCase: EditAddress

--- Appium/testcase/editaddress.py
+++ Appium/testcase/editaddress.py
@@ -5,7 +5,7 @@ import os,sys
 import unittest
 from time import sleep
 from configparser import ConfigParser
-from common import el_click
+from common import el_click,el_send_keys
 
 #config.ini
 cfg = ConfigParser()
@@ -17,6 +17,9 @@ TestCase:
     1. 个人资料--->Add New Address
 
 """
+cnee_name = u'李太白'
+cnee_phone = '18600000000'
+cnee_detail_address = u'景山前街4号'
 
 def add_address(driver):
     sleep(1)
@@ -30,9 +33,19 @@ def add_address(driver):
 
     el_click(driver,cfg.get('nav','my'))
     try:
-        el_click(driver,cfg.get('my','register_mask')
+        el_click(driver,cfg.get('my','register_mask'))
     except NoSuchElementException:
         pass
 
     el_click(driver,cfg.get('my','edit_profile'))
     el_click(driver,cfg.get('personal_data','address'))
+    el_click(driver,cfg.get('address','add_address'))
+
+    el_send_keys(driver,cfg.get('address','cnee_name'),cnee_name)
+    el_send_keys(driver,cfg.get('address','cnee_phone'),cnee_phone)
+    el_click(driver,cfg.get('address','cnee_area'))
+    el_click(driver,cfg.get('address','select_area'))
+    el_send_keys(driver,cfg.get('address','cnee_detail_address'),cnee_detail_address)
+    el_click(driver,cfg.get('address','cnee_commit'))
+
+    el_click(driver,cfg.get('action','back'))
