720fa2dc510edbba407b58591141202d8f1b5ac3
midada <sx.work@outlook.com>
2016-07-19 16:01:53 +0800

    TestCase: EditAddress

--- Appium/testcase/editaddress.py
+++ Appium/testcase/editaddress.py
@@ -5,6 +5,7 @@ import os,sys
 import unittest
 from time import sleep
 from configparser import ConfigParser
+from common import el_click
 
 #config.ini
 cfg = ConfigParser()
@@ -19,10 +20,19 @@ TestCase:
 
 def add_address(driver):
     sleep(1)
-    driver.find_element_by_id(cfg.get('nav','my')).click()
+    # driver.find_element_by_id(cfg.get('nav','my')).click()
+    # try:
+    #     driver.find_element_by_id("com.jiuai:id/linearLayout_mask").click()
+    # except NoSuchElementException:
+    #     pass
+    # driver.find_element_by_id(cfg.get('my','edit_profile')).click()
+    # driver.find_element_by_id(cfg.get('personal_data','address')).click()
+
+    el_click(driver,cfg.get('nav','my'))
     try:
-        driver.find_element_by_id("com.jiuai:id/linearLayout_mask").click()
+        el_click(driver,cfg.get('my','register_mask')
     except NoSuchElementException:
         pass
-    driver.find_element_by_id(cfg.get('my','edit_profile')).click()
-    driver.find_element_by_id(cfg.get('personal_data','address')).click()
+
+    el_click(driver,cfg.get('my','edit_profile'))
+    el_click(driver,cfg.get('personal_data','address'))
