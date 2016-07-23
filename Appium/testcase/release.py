33883579390f247d4b22ead26746ffcb4b3da850
sx.work@outlook.com <sx.work@outlook.com>
2016-07-23 21:21:52 +0800

    TestCase: Release Goods

--- Appium/testcase/release.py
+++ Appium/testcase/release.py
@@ -12,6 +12,7 @@ from configparser import ConfigParser
 from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 
+from common import screenshot
 from common import el_click,el_send_keys,el_xpath_click
 from common import MobileSwipe
 
@@ -33,15 +34,20 @@ def set_goods_type(driver):
 
         //android.widget.ListView[2]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]
     """
-    # set Goods Type
-    el_click(driver,cfg.get('release','goods_type'))
+    try:
+        # set Goods Type
+        el_click(driver,cfg.get('release','goods_type'))
+        screenshot(driver,'screenshot/Release_GoodsType_init_page.png')
 
-    # 分类: 一级
-    driver.find_element_by_xpath('//android.widget.FrameLayout[1]').click()
-
-    #分类：二级
-    el = "//android.widget.ListView[2]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]"
-    el_xpath_click(driver,el)
+        # 分类: 一级
+        driver.find_element_by_xpath('//android.widget.FrameLayout[1]').click()
+        screenshot(driver,'screenshot/Release_GoodsType_first.png')
+        #分类：二级
+        el = "//android.widget.ListView[2]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]"
+        el_xpath_click(driver,el)
+    except:
+        screenshot(driver,'screenshot/Release_GoodsType_error.png')
+        return False
 
 # 添加照片
 def add_photo(driver):
@@ -53,6 +59,8 @@ def add_photo(driver):
             在选择图片页面,从上到下,第一张照片: FrameLayout为[1]
     """
     try:
+        screenshot(driver,'screenshot/Release_Photo_init_page.png')
+
         sleep(1)
         el = "//android.widget.GridView[1]/android.widget.FrameLayout[2]/android.widget.CheckBox"
         #el_xpath_click(driver,e1)
@@ -61,7 +69,9 @@ def add_photo(driver):
     except:
         return False
     else:
+        screenshot(driver,'screenshot/Release_Photo_choice_finish.png')
         el_click(driver,cfg.get('release','ok'))
+        screenshot(driver,'screenshot/Release_Photo_release.png')
 
 # 录制视频
 def add_video(driver):
@@ -76,39 +86,61 @@ def add_video(driver):
             若添加图片视频区域进行了滑动操作,添加视频框输入第一个，则RelativeLayout为[1]
     """
     try:
+        screenshot(driver,'screenshot/Release_Video_init_page.png')
+
         el = "//android.support.v7.widget.RecyclerView[1]/android.widget.RelativeLayout[3]/android.widget.ImageView[1]"
         el_xpath_click(driver,el)
 
-        # click capture Video btn
-        el = driver.find_element_by_id(cfg.get('release','capture_btn'))
+        # find: capture Video btn
+        capture_btn = driver.find_element_by_id(cfg.get('release','capture_btn'))
         # 长按录制10s
         action = TouchAction(driver)
-        action.long_press(el,None,None,10000).perform()
+        action.long_press(capture_btn,None,None,10000).perform()
+
+        screenshot(driver,'screenshot/Release_Video_capture_finish.png')        
+
         sleep(1)
         el_click(driver,cfg.get('release','capture_finish'))
+        screenshot(driver,'screenshot/Release_Video_capture_commit.png')
         sleep(5)
     except:
-        pass
+        try:
+            screenshot(driver,'screenshot/Release_Video_error.png')
+            el_click(driver,cfg.get('release','capture_close'))
+        except:
+            pass
 
 # 选择品牌
 def choice_brand(driver):
-    el_click(driver,cfg.get('release','choice_brand'))
     try:
+        el_click(driver,cfg.get('release','choice_brand'))
+        screenshot(driver,'screenshot/Release_Brand_init_page.png')
+
         el = "//android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]"
         el_xpath_click(driver,el)
     except:
         return False    
+    else:
+        screenshot(driver,'screenshot/Release_Brand_select_finish.png')
 
 # 手机类商品属性规格
 def goods_attribute(driver):
-    el_click(driver,cfg.get('release','goods_attribute'))
+    try:
+        el_click(driver,cfg.get('release','goods_attribute'))
+        screenshot(driver,'screenshot/Release_GoodsAttribute_init_page.png')
 
-    el = "//android.widget.GridView[1]/android.widget.LinearLayout[2]/android.widget.CheckBox[1]"
-    el_xpath_click(driver,el)
-    for c in range(3):
-        sw.down_swipe(driver)
+        el_storage = "//android.widget.GridView[1]/android.widget.LinearLayout[2]/android.widget.CheckBox[1]"
+        el_xpath_click(driver,el_storage)
+        screenshot(driver,'screenshot/Release_GoodsAttribute_choice_storage.png')
 
-    el_click(driver,cfg.get('release','complete'))
+        for c in range(3):
+            sw.down_swipe(driver)
+    except:
+        screenshot(driver,'screenshot/Release_GoodsAttribute_error.png')
+        pass
+    else:
+        el_click(driver,cfg.get('release','complete'))
+        screenshot(driver,'screenshot/Release_GoodsAttribute_select_finish.png')
 
 
 # 商品发布
