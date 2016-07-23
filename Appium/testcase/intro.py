523143f2d68d8f193cea3ad307e6ea26d5bf3682
sx.work@outlook.com <sx.work@outlook.com>
2016-07-23 19:49:04 +0800

    v1.0

--- Appium/testcase/intro.py
+++ Appium/testcase/intro.py
@@ -14,12 +14,13 @@ sw = MobileSwipe()
 def intro(driver):
     try:
         driver.get_screenshot_as_file("screenshot/intro.png")
-        sleep(3)
-        
+        sleep(2)
         for c in range(5):
             sw.left_swipe(driver)
+
         driver.get_screenshot_as_file("screensot/intro_1.png")
         driver.find_element_by_xpath("//android.widget.ImageView").click()
+        sleep(2)
     except:
         pass
 
